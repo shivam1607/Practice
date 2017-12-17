38f21dcbaac30258f5f3b91cf90646ae118692a8
Sharma <shivam.sharma@sap.com>
2017-12-17 21:24:54 +0530

    Initial commit

--- src/Test/Check.java
+++ src/Test/Check.java
@@ -1,7 +1,14 @@
 package Test;
 
+import static java.lang.System.out;
+
 /**
  * Created by I329889 on 12/17/2017.
  */
 public class Check {
+
+    public static void main(String[] args)
+    {
+        out.println("This is my first program in IntelliJ");
+    }
 }
